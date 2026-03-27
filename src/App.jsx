c3a5644239c7b5bd1e5f5fa9dc5e2d22b1dfd037
Edems <edems0147659277@gmail.com>
2026-03-27 23:05:54 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import PaymentSuccess from './pages/PaymentSuccess';
 import APropos from './pages/APropos';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
